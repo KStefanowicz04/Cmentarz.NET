06c2e5fa99c7233e4ebc7adc8262c5e8f162bc35
Lukasz97947 <91849@student.pb.edu.pl>
2026-03-10 20:45:19 +0100

    Dodanie klasy grave

--- ProjektCmentarz/Models/DeceasedModel.cs
+++ ProjektCmentarz/Models/DeceasedModel.cs
@@ -24,4 +24,19 @@ namespace ProjektCmentarz.Models
         // Data śmierci nieboszczyka
         public DateTime DeathDate { get; set; }
     }
+        //grob
+        public class Grave
+    {
+        [Key]
+        public int Id { get; set; }
+    
+        // Numer grobu
+        public string GraveNumber { get; set; }
+    
+        // Sektor cmentarza
+        public string Sector { get; set; }
+    
+        // Relacja ze zmarłym (Wiecej niz jeden grób rodzinny)
+        public List<Deceased> Deceased { get; set; }
+    }
 }
