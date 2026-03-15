02f7f6c977c96004c0f086a902547c3fa33218fd
Karolina <karolina.czaplicka4@gmail.com>
2026-03-15 13:23:23 +0100

    SectionType, GraveType

---
+++ ProjektCmentarz/Models/GraveType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjektCmentarz.Models
+{
+    public class GraveType
+    {
+        // Id typu grobu; klucz głóny 
+        [Key]
+        public int Id { get; set; }
+
+        //Nazwa typu grobu 
+        [Required(ErrorMessage = "Grave type name is required")]
+        [StringLength(100)]
+        public string Name { get; set; }    
+    }
+}
