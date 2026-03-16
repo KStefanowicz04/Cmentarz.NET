d2d6a59172f1d138d29255a780e0bdc093b7a00b
Lukasz97947 <91849@student.pb.edu.pl>
2026-03-16 06:42:05 +0100

    Uzupelniwnie klasy Urn

--- ProjektCmentarz/Models/Urn.cs
+++ ProjektCmentarz/Models/Urn.cs
@@ -1,6 +1,36 @@
-﻿namespace ProjektCmentarz.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjektCmentarz.Models
 {
     public class Urn
     {
+        [Key]
+        public int Id { get; set; }
+
+        // Model lub nazwa urny 
+        [Required(ErrorMessage = "Urn model name is required")]
+        [StringLength(100)]
+        public string ModelName { get; set; } = string.Empty;
+
+        // Materiał wykonania (np. Kamień, Drewno, Metal, Kompozyt)
+        [Required]
+        [StringLength(50)]
+        public string Material { get; set; } = string.Empty;
+
+        // Pojemność w litrach 
+        [Range(0.1, 10.0)]
+        public double Capacity { get; set; }
+
+        // Cena
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        // Jeden pogrzeb to jedna urna, dodajemy klucz obcy do Funeral
+        public int? FuneralId { get; set; }
+
+        [ForeignKey("FuneralId")]
+        public virtual Funeral? Funeral { get; set; }
     }
 }
