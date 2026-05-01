1906cefb91f28e9e64021e587cc61ff08ac1d70a
KStefanowicz04 <kacper.stefanowicz04@gmail.com>
2026-05-01 19:28:47 +0200

    Podstawowy profil użytkownika ze stroną do edytowania danych

---
+++ ProjektCmentarz/Controllers/UserProfileController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging.Signing;
+using ProjektCmentarz.Data;
+using ProjektCmentarz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektCmentarz.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private readonly GraveyardContext _context;
+
+        public UserProfileController(GraveyardContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserProfile
+        [Authorize()]
+        public async Task<IActionResult> Index()
+        {
+            // ID Użytkownika jest pobierane z Claimu UserID przypisanego przy logowaniu
+            int userId = int.Parse(User.FindFirst("UserId").Value);
+
+            // Wybieramy użytkownika z bazy danych o tym samym ID co zalogowany użytkownik
+            var user = await _context.Users
+                .Include(u => u.Roles)
+                .FirstOrDefaultAsync(u => u.UserId == userId);
+
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+
+        // Edytowanie swoich własnych danych przez Użytkownika.
+        // GET: /UserProfile/Edit
+        public async Task<IActionResult> Edit()
+        {
+            int userId = int.Parse(User.FindFirst("UserId").Value);
+
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+
+        // POST: /UserProfile/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(UserProfileEdit userProfileModel)
+        {
+            if (!ModelState.IsValid)
+                return View(userProfileModel);
+
+            var user = await _context.Users.FindAsync(userProfileModel.UserId);
+            if (user == null)
+                return NotFound();
+
+            // Zmiana podstawowych danych, tu nie można zmienić hasła
+            user.FirstName = userProfileModel.FirstName;
+            user.Surname = userProfileModel.Surname;
+            user.Email = userProfileModel.Email;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ ProjektCmentarz/Models/UserProfileEditModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjektCmentarz.Models
+{
+    // Model służący do edytowania danych użytkownika
+    public class UserProfileEdit
+    {
+        // ID użytkownika; klucz główny
+        [Key]
+        public int UserId { get; set; }
+
+        // Imię użytkownika
+        [Display(Name = "Imię")]
+        [Required(ErrorMessage = "Imię użytkownika jest wymagane.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Imię ma od 1 do 100 znaków.")]
+        public string FirstName { get; set; }
+
+        // Nazwisko użytkownika
+        [Display(Name = "Nazwisko")]
+        [Required(ErrorMessage = "Nazwisko jest wymagane.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Nazwisko ma od 1 do 100 znaków.")]
+        public string Surname { get; set; }
+
+        // Email użytkownika
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email jest wymagany.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Email ma od 1 do 100 znaków.")]
+        public string Email { get; set; }
+    }
+}
