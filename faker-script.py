0fe08e56c03664d63907397a9450ec8e3f5a7e0d
KStefanowicz04 <kacper.stefanowicz04@gmail.com>
2026-05-05 20:31:28 +0200

    Więcej sekcji dodawania danych do DB w faker-script.py

--- faker-script.py
+++ faker-script.py
@@ -64,9 +64,8 @@ if conn:
 
 # # Wypełnienie tabeli Priests losowymi danymi: imie, nazwisko, nowe ContactData dla danego księdza, id losowej parafii.
 # for i in range(N):
-#     full_name = fake.name().split(' ')
-#     name = full_name[0]
-#     surname = full_name[1]
+#     name = fake.first_name()
+#     surname = fake.last_name()
 #     ## Po podstawowych danych tworzone jest nowe ContactData dla danego Księdza
 #     phone = fake.phone_number()
 #     email = fake.email()
@@ -98,3 +97,89 @@ if conn:
 
 # conn.commit()
 # print(f"Wstawiono {i+1} księży!")
+
+
+
+# # Wypełnienie tabeli PlotOwners losowymi danymi: imie, nazwisko, nowe ContactData
+# for i in range(N):
+#     name = fake.first_name()
+#     surname = fake.last_name()
+#     ## Po podstawowych danych tworzone jest nowe ContactData dla właściciela
+#     phone = fake.phone_number()
+#     email = fake.email()
+#     city = fake.city()
+#     street = fake.street_address()
+#     post_code = fake.postcode()
+#     cursor.execute(
+#         """
+#         INSERT INTO ContactDatas (PhoneNumber, EMail, CityName, StreetName, ZipCode)
+#         OUTPUT INSERTED.Id
+#         VALUES (?, ?, ?, ?, ?)
+#         """,
+#         phone, email, city, street, post_code
+#     )
+#     contact_data_id = cursor.fetchone()[0]
+
+#     cursor.execute(
+#         """
+#         INSERT INTO PlotOwners (FirstName, Surname, ContactDataId)
+#         VALUES (?, ?, ?)
+#         """,
+#         name, surname, contact_data_id
+#     )
+
+# conn.commit()
+# print(f"Wstawiono {i+1} właścicieli działek!")
+
+
+
+# # Wypełnienie tabeli Plots losowymi danymi: właściciel działki, sekcja cmentarza
+# for i in range(N):
+#     ## Wybrany zostanie losowy właściciel działki
+#     cursor.execute("SELECT Id FROM PlotOwners")
+#     contact_data_ids = [row[0] for row in cursor.fetchall()]
+#     contact_data_id = random.choice(contact_data_ids)
+
+#     ## Wybrana zostanie losowa sekcja cmentarza
+#     cursor.execute("SELECT Id FROM GraveyardSection")
+#     graveyard_section_ids = [row[0] for row in cursor.fetchall()]
+#     graveyard_section_id = random.choice(graveyard_section_ids)
+
+#     cursor.execute(
+#         """
+#         INSERT INTO Plots (PlotOwnerId, GraveyardSectionId)
+#         VALUES (?, ?)
+#         """,
+#         contact_data_id, graveyard_section_id
+#     )
+
+# conn.commit()
+# print(f"Wstawiono {i+1} działek!")
+
+
+
+# Wypełnienie tabeli Funerals losowymi danymi: data odbycia pogrzebu, nieboszczyk, ksiądz, dom pogrzebowy, działka odbycia pogrzebu
+for i in range(5):
+    ## Wybrany zostanie losowa data
+    date = fake.date();
+
+    ## Wybrany zostanie losowy nieboszczyk niemający jeszcze pogrzebu
+    cursor.execute("SELECT Id FROM Deceaseds")
+    deceaseds = [row[0] for row in cursor.fetchall()]
+    deceased = random.choice(deceaseds)
+
+    ## Wybrana zostanie losowa działka
+    cursor.execute("SELECT Id FROM Plots")
+    plot_ids = [row[0] for row in cursor.fetchall()]
+    plot_id = random.choice(plot_ids)
+
+    cursor.execute(
+        """
+        INSERT INTO Funerals (FuneralDate, DeceasedId, PriestId, FuneralHomeId, PlotId)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        date, a, b, c, plot_id
+    )
+
+conn.commit()
+print(f"Wstawiono {i+1} działek!")
