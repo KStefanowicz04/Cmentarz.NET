9276f48b8121c08c5f9c1b9871bbbc5ff8e18c9b
KStefanowicz04 <kacper.stefanowicz04@gmail.com>
2026-05-09 20:44:12 +0200

    Dodawanie użytkowników, poprawiony skrypt faker-script.py

--- faker-script.py
+++ faker-script.py
@@ -3,6 +3,7 @@ import pyodbc
 import random
 from random import randint
 from datetime import timedelta 
+import hashlib
 
 fake = Faker('pl_PL')
 
@@ -253,6 +254,7 @@ print(f"Wypełniono słownik Roles!")
 
 
 ## Zwykłe tabele
+## NIE POTRZEBNE. Wszystkie tabele które wymagają jakiegoś ContactData tworzą je same
 # # Wypełnienie tabeli ContactData losowymi danymi kontaktowymi: #telefonu, email, miasto, ulica, kod pocztowy
 # for i in range(N):
 #     phone = fake.phone_number()
@@ -274,99 +276,120 @@ print(f"Wypełniono słownik Roles!")
 
 
 
-# # Wypełnienie tabeli Priests losowymi danymi: imie, nazwisko, nowe ContactData dla danego księdza, id losowej parafii.
-# for i in range(N):
-#     name = fake.first_name()
-#     surname = fake.last_name()
-#     ## Po podstawowych danych tworzone jest nowe ContactData dla danego Księdza
-#     phone = fake.phone_number()
-#     email = fake.email()
-#     city = fake.city()
-#     street = fake.street_address()
-#     post_code = fake.postcode()
-#     cursor.execute(
-#         """
-#         INSERT INTO ContactDatas (PhoneNumber, EMail, CityName, StreetName, ZipCode)
-#         OUTPUT INSERTED.Id
-#         VALUES (?, ?, ?, ?, ?)
-#         """,
-#         phone, email, city, street, post_code
-#     )
-#     contact_data_id = cursor.fetchone()[0]
-    
-#     ## Wybrana zostanie losowa parafia
-#     cursor.execute("SELECT Id FROM Parishes")
-#     parish_ids = [row[0] for row in cursor.fetchall()]
-#     parish_id = random.choice(parish_ids)
+# Wypełnienie tabeli Priests losowymi danymi: imie, nazwisko, nowe ContactData dla danego księdza, id losowej parafii.
+## Liczenie liczby Księży; w bazie będzie znajdować się najwyżej N Księży
+cursor.execute("SELECT COUNT(*) FROM Priests")
+current_count = cursor.fetchone()[0]
 
-#     cursor.execute(
-#         """
-#         INSERT INTO Priests (FirstName, Surname, ContactDataId, ParishId)
-#         VALUES (?, ?, ?, ?)
-#         """,
-#         name, surname, contact_data_id, parish_id
-#     )
+if current_count < N:
+    remaining = N - current_count
 
-# conn.commit()
-# print(f"Wstawiono {i+1} księży!")
+    for i in range(remaining):
+        name = fake.first_name()
+        surname = fake.last_name()
+        ## Po podstawowych danych tworzone jest nowe ContactData dla danego Księdza
+        phone = fake.phone_number()
+        email = f"{name.lower()}.{surname.lower()}@example.com"
+        city = fake.city()
+        street = fake.street_address()
+        post_code = fake.postcode()
+        cursor.execute(
+            """
+            INSERT INTO ContactDatas (PhoneNumber, EMail, CityName, StreetName, ZipCode)
+            OUTPUT INSERTED.Id
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            phone, email, city, street, post_code
+        )
+        contact_data_id = cursor.fetchone()[0]
+        
+        ## Wybrana zostanie losowa parafia
+        cursor.execute("SELECT Id FROM Parishes")
+        parish_ids = [row[0] for row in cursor.fetchall()]
+        parish_id = random.choice(parish_ids)
 
+        cursor.execute(
+            """
+            INSERT INTO Priests (FirstName, Surname, ContactDataId, ParishId)
+            VALUES (?, ?, ?, ?)
+            """,
+            name, surname, contact_data_id, parish_id
+        )
 
-
-# # Wypełnienie tabeli PlotOwners losowymi danymi: imie, nazwisko, nowe ContactData
-# for i in range(N):
-#     name = fake.first_name()
-#     surname = fake.last_name()
-#     ## Po podstawowych danych tworzone jest nowe ContactData dla właściciela
-#     phone = fake.phone_number()
-#     email = fake.email()
-#     city = fake.city()
-#     street = fake.street_address()
-#     post_code = fake.postcode()
-#     cursor.execute(
-#         """
-#         INSERT INTO ContactDatas (PhoneNumber, EMail, CityName, StreetName, ZipCode)
-#         OUTPUT INSERTED.Id
-#         VALUES (?, ?, ?, ?, ?)
-#         """,
-#         phone, email, city, street, post_code
-#     )
-#     contact_data_id = cursor.fetchone()[0]
-
-#     cursor.execute(
-#         """
-#         INSERT INTO PlotOwners (FirstName, Surname, ContactDataId)
-#         VALUES (?, ?, ?)
-#         """,
-#         name, surname, contact_data_id
-#     )
-
-# conn.commit()
-# print(f"Wstawiono {i+1} właścicieli działek!")
+    conn.commit()
+    print(f"Wstawiono {i+1} księży!")
 
 
 
-# # Wypełnienie tabeli Plots losowymi danymi: właściciel działki, sekcja cmentarza
-# for i in range(N):
-#     ## Wybrany zostanie losowy właściciel działki
-#     cursor.execute("SELECT Id FROM PlotOwners")
-#     contact_data_ids = [row[0] for row in cursor.fetchall()]
-#     contact_data_id = random.choice(contact_data_ids)
+# Wypełnienie tabeli PlotOwners losowymi danymi: imie, nazwisko, nowe ContactData
+## Liczenie liczby Właścicieli; w bazie będzie znajdować się najwyżej N Właścicieli
+cursor.execute("SELECT COUNT(*) FROM PlotOwners")
+current_count = cursor.fetchone()[0]
 
-#     ## Wybrana zostanie losowa sekcja cmentarza
-#     cursor.execute("SELECT Id FROM GraveyardSection")
-#     graveyard_section_ids = [row[0] for row in cursor.fetchall()]
-#     graveyard_section_id = random.choice(graveyard_section_ids)
+if current_count < N:
+    remaining = N - current_count
 
-#     cursor.execute(
-#         """
-#         INSERT INTO Plots (PlotOwnerId, GraveyardSectionId)
-#         VALUES (?, ?)
-#         """,
-#         contact_data_id, graveyard_section_id
-#     )
+    for i in range(remaining):
+        name = fake.first_name()
+        surname = fake.last_name()
+        ## Po podstawowych danych tworzone jest nowe ContactData dla właściciela
+        phone = fake.phone_number()
+        email = f"{name.lower()}.{surname.lower()}@example.com"
+        city = fake.city()
+        street = fake.street_address()
+        post_code = fake.postcode()
+        cursor.execute(
+            """
+            INSERT INTO ContactDatas (PhoneNumber, EMail, CityName, StreetName, ZipCode)
+            OUTPUT INSERTED.Id
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            phone, email, city, street, post_code
+        )
+        contact_data_id = cursor.fetchone()[0]
 
-# conn.commit()
-# print(f"Wstawiono {i+1} działek!")
+        cursor.execute(
+            """
+            INSERT INTO PlotOwners (FirstName, Surname, ContactDataId)
+            VALUES (?, ?, ?)
+            """,
+            name, surname, contact_data_id
+        )
+
+    conn.commit()
+    print(f"Wstawiono {i+1} właścicieli działek!")
+
+
+
+# Wypełnienie tabeli Plots losowymi danymi: właściciel działki, sekcja cmentarza
+## Liczenie liczby Działek; w bazie będzie znajdować się najwyżej N Działek
+cursor.execute("SELECT COUNT(*) FROM Plots")
+current_count = cursor.fetchone()[0]
+
+if current_count < N:
+    remaining = N - current_count
+
+    for i in range(remaining):
+        ## Wybrany zostanie losowy właściciel działki
+        cursor.execute("SELECT Id FROM PlotOwners")
+        contact_data_ids = [row[0] for row in cursor.fetchall()]
+        contact_data_id = random.choice(contact_data_ids)
+
+        ## Wybrana zostanie losowa sekcja cmentarza
+        cursor.execute("SELECT Id FROM GraveyardSection")
+        graveyard_section_ids = [row[0] for row in cursor.fetchall()]
+        graveyard_section_id = random.choice(graveyard_section_ids)
+
+        cursor.execute(
+            """
+            INSERT INTO Plots (PlotOwnerId, GraveyardSectionId)
+            VALUES (?, ?)
+            """,
+            contact_data_id, graveyard_section_id
+        )
+
+        conn.commit()
+        print(f"Wstawiono {i+1} działek!")
 
 
 
@@ -374,102 +397,160 @@ print(f"Wypełniono słownik Roles!")
 # Również tworzy nowy Casket i wypełnia losowymi danymi: materiał, cena
 # Również tworzy nowy Funeral i wypełnia losowymi danymi: data odbycia pogrzebu, nieboszczyk, ksiądz, dom pogrzebowy, działka odbycia pogrzebu
 # Również tworzy nowy Grave i wypełnia losowymi danymi: działa, nieboszczyk, głębokość grobu
-#     for i in range(200):
-#         ## Tworzenie nieboszczyka
-#         name = fake.first_name()
-#         surname = fake.last_name()
-#         birth_date = fake.date_of_birth()
-#         death_date = birth_date + timedelta(days=randint(8000, 30000))
+## Liczenie liczby Nieboszczyków; w bazie będzie znajdować się najwyżej N Nieboszczyków
+cursor.execute("SELECT COUNT(*) FROM Deceaseds")
+current_count = cursor.fetchone()[0]
 
-#         ## Utworzenie Nieboszczyka
-#         cursor.execute(
-#             """
-#             INSERT INTO Deceaseds (FirstName, Surname, BirthDate, DeathDate)
-#             OUTPUT INSERTED.Id
-#             VALUES (?, ?, ?, ?)
-#             """,
-#             name, surname, birth_date, death_date
-#         )
-#         ## ID nowo utworzonego nieboszczyka
-#         deceased_id = cursor.fetchone()[0]
+if current_count < N:
+    remaining = N - current_count
 
+    for i in range(remaining):
+        ## Tworzenie nieboszczyka
+        name = fake.first_name()
+        surname = fake.last_name()
+        birth_date = fake.date_of_birth()
+        death_date = birth_date + timedelta(days=randint(8000, 30000))
 
-#         ## Wypełnienie brakujących danych: trumna (CasketId) i pogrzeb (FuneralId)
-
-#         ## Utworzenie losowej trumny
-#         # Najpierw trzeba mieć liczbę Materiałów w tabeli słownikowej Materials
-#         cursor.execute("SELECT Id FROM Materials")
-#         material_ids = [row[0] for row in cursor.fetchall()]
-#         casket_material_id = random.choice(material_ids)
-#         casket_price = randint(100, 9999)
-#         ## Utworzenie trumny
-#         cursor.execute(
-#             """
-#             INSERT INTO Casket (MaterialId, Price, DeceasedId)
-#             OUTPUT INSERTED.Id
-#             VALUES (?, ?, ?)
-#             """,
-#             casket_material_id, casket_price, deceased_id
-#         )
-#         ## ID nowo utworzonego grobu
-#         casket_id = cursor.fetchone()[0]
+        ## Utworzenie Nieboszczyka
+        cursor.execute(
+            """
+            INSERT INTO Deceaseds (FirstName, Surname, BirthDate, DeathDate)
+            OUTPUT INSERTED.Id
+            VALUES (?, ?, ?, ?)
+            """,
+            name, surname, birth_date, death_date
+        )
+        ## ID nowo utworzonego nieboszczyka
+        deceased_id = cursor.fetchone()[0]
 
 
-#         ## Utworzenie losowego pogrzebu
-#         # Wybrana zostanie data 3 dni po śmierci nieboszczyka
-#         date = death_date + timedelta(days=3)
+        ## Wypełnienie brakujących danych: trumna (CasketId) i pogrzeb (FuneralId)
 
-#         # Wybrany zostanie losowy ksiądz
-#         cursor.execute("SELECT Id FROM Priests")
-#         priest_ids = [row[0] for row in cursor.fetchall()]
-#         priest_id = random.choice(priest_ids)
-
-#         # Wybrany zostanie losowy Dom Pogrzebowy
-#         cursor.execute("SELECT Id FROM FuneralHomes")
-#         funeral_home_ids = [row[0] for row in cursor.fetchall()]
-#         funeral_home_id = random.choice(funeral_home_ids)
-
-#         # Wybrana zostanie losowa działka
-#         cursor.execute("SELECT Id FROM Plots")
-#         plot_ids = [row[0] for row in cursor.fetchall()]
-#         plot_id = random.choice(plot_ids)
-
-#         cursor.execute(
-#             """
-#             INSERT INTO Funerals (FuneralDate, DeceasedId, PriestId, FuneralHomeId, PlotId)
-#             OUTPUT INSERTED.Id
-#             VALUES (?, ?, ?, ?, ?)
-#             """,
-#             date, deceased_id, priest_id, funeral_home_id, plot_id
-#         )
-#         ## ID nowo utworzonego pogrzebu
-#         funeral_id = cursor.fetchone()[0]
+        ## Utworzenie losowej trumny
+        # Najpierw trzeba mieć liczbę Materiałów w tabeli słownikowej Materials
+        cursor.execute("SELECT Id FROM Materials")
+        material_ids = [row[0] for row in cursor.fetchall()]
+        casket_material_id = random.choice(material_ids)
+        casket_price = randint(100, 9999)
+        ## Utworzenie trumny
+        cursor.execute(
+            """
+            INSERT INTO Casket (MaterialId, Price, DeceasedId)
+            OUTPUT INSERTED.Id
+            VALUES (?, ?, ?)
+            """,
+            casket_material_id, casket_price, deceased_id
+        )
+        ## ID nowo utworzonego grobu
+        casket_id = cursor.fetchone()[0]
 
 
-#         ## Utworzenie losowego grobu na danej działce dla danego nieboszczyka
-#         # Wybrana zostanie losowa głębokość grobu
-#         cursor.execute("SELECT Id FROM BurialDepth")
-#         depth_ids = [row[0] for row in cursor.fetchall()]
-#         depth_id = random.choice(depth_ids)
+        ## Utworzenie losowego pogrzebu
+        # Wybrana zostanie data 3 dni po śmierci nieboszczyka
+        date = death_date + timedelta(days=3)
 
-#         cursor.execute(
-#             """
-#             INSERT INTO Graves (PlotId, DeceasedId, BurialDepthId)
-#             VALUES (?, ?, ?)
-#             """,
-#             plot_id, deceased_id, depth_id
-#         )
+        # Wybrany zostanie losowy ksiądz
+        cursor.execute("SELECT Id FROM Priests")
+        priest_ids = [row[0] for row in cursor.fetchall()]
+        priest_id = random.choice(priest_ids)
 
-#         ## Przypisanie FuneralId i GraveId do Deceased
-#         cursor.execute(
-#             """
-#             UPDATE Deceaseds
-#             SET CasketId = ?, FuneralId = ?
-#             WHERE Id = ?
-#             """,
-#             (casket_id, funeral_id, deceased_id)
-#         )
+        # Wybrany zostanie losowy Dom Pogrzebowy
+        cursor.execute("SELECT Id FROM FuneralHomes")
+        funeral_home_ids = [row[0] for row in cursor.fetchall()]
+        funeral_home_id = random.choice(funeral_home_ids)
+
+        # Wybrana zostanie losowa działka
+        cursor.execute("SELECT Id FROM Plots")
+        plot_ids = [row[0] for row in cursor.fetchall()]
+        plot_id = random.choice(plot_ids)
+
+        cursor.execute(
+            """
+            INSERT INTO Funerals (FuneralDate, DeceasedId, PriestId, FuneralHomeId, PlotId)
+            OUTPUT INSERTED.Id
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            date, deceased_id, priest_id, funeral_home_id, plot_id
+        )
+        ## ID nowo utworzonego pogrzebu
+        funeral_id = cursor.fetchone()[0]
 
 
-#     conn.commit()
-# print(f"Wstawiono {i+1} nieboszczyków, pogrzebów, grobów!")
+        ## Utworzenie losowego grobu na danej działce dla danego nieboszczyka
+        # Wybrana zostanie losowa głębokość grobu
+        cursor.execute("SELECT Id FROM BurialDepth")
+        depth_ids = [row[0] for row in cursor.fetchall()]
+        depth_id = random.choice(depth_ids)
+
+        cursor.execute(
+            """
+            INSERT INTO Graves (PlotId, DeceasedId, BurialDepthId)
+            VALUES (?, ?, ?)
+            """,
+            plot_id, deceased_id, depth_id
+        )
+
+        ## Przypisanie FuneralId i GraveId do Deceased
+        cursor.execute(
+            """
+            UPDATE Deceaseds
+            SET CasketId = ?, FuneralId = ?
+            WHERE Id = ?
+            """,
+            (casket_id, funeral_id, deceased_id)
+        )
+
+
+    conn.commit()
+    print(f"Wstawiono {i+1} nieboszczyków, pogrzebów, grobów!")
+
+
+# Wypełnienie tabeli Users losowymi zwykłymi użytkownikami. Dodaj również użytkownika admin z rolą Admin.
+# Bez zalogowania na to konto dostęp do niektórych stron jest ograniczony.
+## Funkcja do hashowania hasła
+def hash_password(password: str) -> bytes:
+    return hashlib.sha256(password.encode("utf-8")).digest()
+
+## Liczenie liczby użytkowników; w bazie będzie znajdować się najwyżej N użytkowników
+cursor.execute("SELECT COUNT(*) FROM Users")
+current_count = cursor.fetchone()[0]
+
+if current_count < N:
+    remaining = N - current_count
+
+    # Zebranie ID roli "Użytkownik" w bazie
+    cursor.execute("SELECT Id FROM Roles WHERE RoleName = N'Użytkownik'")
+    uzytkownik_role_id = cursor.fetchone()[0]
+
+    for i in range(remaining):
+        name = fake.first_name()
+        surname = fake.last_name()
+        email = f"{name.lower()}.{surname.lower()}@example.com"
+        password_string = fake.word()
+        ## Hashowanie hasła (jest i musi być TEN SAM SPOSÓB co w Kontrolerze do Logowania i Kontrolerze do Rejestracji)
+        password = hash_password(password_string)
+
+        print(f"Imie: {name}, Nazwisko: {surname}, EMail: {email,} Hasło: {password_string}, Hash: {password}\n")
+
+        ## Dodanie użytkownika
+        cursor.execute(
+            """
+            INSERT INTO Users (FirstName, Surname, Email, Password)
+            OUTPUT INSERTED.UserId
+            VALUES (?, ?, ?, ?)
+            """,
+            name, surname, email, password
+        )
+        user_id = cursor.fetchone()[0]
+
+        ## Dodanie roli Użytkownik dla danego użytkownika
+        cursor.execute(
+            """
+            INSERT INTO RoleUser (RolesId, UsersUserId)
+            VALUES (?, ?)
+            """,
+            uzytkownik_role_id, user_id
+        )
+
+    conn.commit()
+    print(f"Wstawiono {i+1} użytkowników!")
