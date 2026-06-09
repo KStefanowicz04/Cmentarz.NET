aa12e96e19a39db7d9894a7695e08e74a02be57e
Karolina <karolina.czaplicka4@gmail.com>
2026-06-09 18:19:20 +0200

    zmiana strony z błeami, strony z Privacy, oraz Forbidden.  Dodanie możliwości usuwania działek. Zmiany w stornie głównej. Dodanie strony z działakmi. Edycja ztron z profilme użytkownika oraz logowaniem.

---
+++ ProjektCmentarz/Views/Home/Forbidden.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjektCmentarz.Views.Home
+{
+    public class ForbiddenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
